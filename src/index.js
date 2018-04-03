b98e3bd8b594b55bf0c1e54c29861d2786a0de16
kojaniydracon <kojaniydracon@gmail.com>
2018-04-03 20:49:45 +0300

    feature:awesomed

--- src/index.js
+++ src/index.js
@@ -1,18 +1,102 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (property) => {
+
+Object.defineProperty(Object.prototype, property, {
+    value: 'someEpicName',
+    enumerable: true, //it doesn't influence, I don't know why???
+    configurable: true,
+    writable: true
+
+});
+return property;
+
+
+};
+const createNotEnumerableProperty = (property) => {
+    Object.defineProperty(Object.prototype,property,{
+
+        get: function() { return _prop; },
+        set: function(property) { _prop = property; },
+
+        enumerable: true, //It doesn't influence,, I don't know why??? It's just a magic.
+
+    });
+    return property;
+};
+const createProtoMagicObject = () => {
+    function func() {}
+    func.prototype = func.__proto__;
+    return func;
+};
+const incrementor = () => {
+    incrementor.privateCounter = incrementor.privateCounter || 0;
+    function counter() {
+        incrementor.privateCounter++;
+        return counter;
+    }
+
+    counter.valueOf = () => incrementor.privateCounter;
+
+    return counter();
+};
+const asyncIncrementor = () => {
+    asyncIncrementor.privateCounter = asyncIncrementor.privateCounter || 0;
+    return new Promise(resolve => {
+        setTimeout(() => {
+            asyncIncrementor.privateCounter++;
+            resolve(asyncIncrementor.privateCounter);
+        }, 5)
+    })
+};
+const createIncrementer = () => {
+
+    return {
+        [Symbol.iterator](){
+            return this;
+        },
+        next(){
+            return {
+                value: this.count > 15 ? undefined : ++this.count || (this.count = 1),
+                done: this.count > 15
+            }
+        }
+    }
+
+
+
+
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = (num) => {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(num);
+        },1000)
+    })
+};
+const getDeepPropertiesCount = (obj) => {
+    let count = 0;
+    function getDeepProp(obj) {
+        for(let key in obj){
+            if (typeof obj[key] === 'object'){
+                getDeepProp(obj[key]);
+                count++;
+            }
+
+        }
+        return count;
+    }
+    return getDeepProp(obj);
+};
+const createSerializedObject = () => {
+    return null;
+};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (arr) => {
+    return arr.sort((left, right) => left.__proto__ - right.__proto__)
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
